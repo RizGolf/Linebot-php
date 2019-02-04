5c8b4c51b30a0b3c4dc6fe2751d7d583ef841d78
RizGolf <46455736+RizGolf@users.noreply.github.com>
2019-02-05 00:30:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,24 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
   // Loop through each event
   foreach ($events['events'] as $event) {
-      // Line API send a lot of event type, we interested in message only.
-      if ($event['type'] == 'message') {
-        switch($event['message']['type']) {
-          case 'text':
-          // Get replyToken
-          $replyToken = $event['replyToken'];
-        
-          // Reply message
-          $respMessage = 'Hello, your message is '. $event['message']['text'];
-            
+   
+      // Get replyToken
+      $replyToken = $event['replyToken'];
+      $ask = $event['message']['text'];
+    
+          switch(strtolower($ask)) {
+              case 'm':
+                $respMessage = 'What sup man. Go away!';
+                break;
+              case 'f':
+                $respMessage = 'Love you lady.';
+                break;
+              default:
+                $respMessage = 'What is your sex? M or F';
+                break;
+              
+              
+              
           $httpClient = new CurlHTTPClient($channel_token);
           $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));  
           $textMessageBuilder = new TextMessageBuilder($respMessage); 
@@ -34,5 +42,4 @@ if (!is_null($events['events'])) {
         }
       }
   }
-}
 echo 'OK';
